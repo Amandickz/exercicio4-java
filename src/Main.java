9651d348a5ef643f66c10cd7b35d426b2eb1c483
Amandickz <amandickz@gmail.com>
2024-11-27 21:44:32 -0300

    Inserção do Exercício 2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		//Exercício 1
-		System.out.println("--- Exercício 1 - Acesso ---");
+		System.out.println("--- Exercício 1 - Ímpares ---");
 		Exercicio1 exe1 = new Exercicio1();
 		exe1.execucao();
+		
+		//Exercício 2
+		System.out.println("--- Exercício 2 - Valores In e Out ---");
+		Exercicio2 exe2 = new Exercicio2();
+		exe2.execucao();
 		
 		System.exit(0);
 		
